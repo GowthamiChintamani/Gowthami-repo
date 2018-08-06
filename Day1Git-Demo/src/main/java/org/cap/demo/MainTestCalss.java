225f259ac924b6c21ca35d80ba5a442060819efe
gchintam <gchintam@DIN73002599.corp.capgemini.com>
2018-08-06 12:20:56 +0530

    Addition of two num

--- Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
+++ Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
@@ -3,9 +3,11 @@ package org.cap.demo;
 public class MainTestCalss {
 public static void main(String[] args) {
 	SalesClass class1=new SalesClass();
+	Addition add=new Addition();
 	System.out.println("hello world");
 	greet();
 	class1.show();
+	add.addShow(4,5);
 }
 
 private static void greet() {
