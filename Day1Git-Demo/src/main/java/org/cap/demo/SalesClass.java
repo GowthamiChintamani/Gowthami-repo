e91ba2faad5f7265ebe10165bb46d73f5528bcac
sgunnala <sgunnala@DIN73002727.corp.capgemini.com>
2018-08-06 14:39:01 +0530

    sum of array elements

--- Day1Git-Demo/src/main/java/org/cap/demo/SalesClass.java
+++ Day1Git-Demo/src/main/java/org/cap/demo/SalesClass.java
@@ -1,11 +1,13 @@
 package org.cap.demo;
 
 public class SalesClass {
-	int[] arr= {1,2,3,4,5};
+	int[] arr= {1,2,3,4,5}; 
+	int sum=0;
 public void show() {
 	for (int i : arr) {
+		sum+=i;
 		System.out.println(i);
 	}
-
+System.out.println(sum);
 }
 }
