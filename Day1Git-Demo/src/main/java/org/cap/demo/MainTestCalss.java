171655668bf9ab36028c63b11bddcd01abd77395
gchintam <gchintam@DIN73002599.corp.capgemini.com>
2018-08-06 10:30:46 +0530

    My First Commit

---
+++ Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainTestCalss {
+public static void main(String[] args) {
+	System.out.println("hello world");
+	greet();
+}
+
+private static void greet() {
+	System.out.println("hello");
+	
+}
+}
