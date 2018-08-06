a3f287e4685d28bf1401b3304919f308b85cab9e
gchintam <gchintam@DIN73002599.corp.capgemini.com>
2018-08-06 12:27:17 +0530

    subtraction class added

--- Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
+++ Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
@@ -3,9 +3,11 @@ package org.cap.demo;
 public class MainTestCalss {
 public static void main(String[] args) {
 	SalesClass class1=new SalesClass();
+	Subraction sub=new Subraction();
 	System.out.println("hello world");
 	greet();
 	class1.show();
+	sub.subnum(5,2);
 }
 
 private static void greet() {
