88acd902b88f09cfabbad5b2a3f89e02bc9798ba
gchintam <gchintam@DIN73002599.corp.capgemini.com>
2018-08-06 12:31:10 +0530

    last commit

--- Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
+++ Day1Git-Demo/src/main/java/org/cap/demo/MainTestCalss.java
@@ -3,19 +3,19 @@ package org.cap.demo;
 public class MainTestCalss {
 public static void main(String[] args) {
 	SalesClass class1=new SalesClass();
-<<<<<<< HEAD
+
 	Subraction sub=new Subraction();
-=======
+
 	Addition add=new Addition();
->>>>>>> refs/heads/subtraction
+
 	System.out.println("hello world");
 	greet();
 	class1.show();
-<<<<<<< HEAD
+
 	sub.subnum(5,2);
-=======
+
 	add.addShow(4,5);
->>>>>>> refs/heads/subtraction
+
 }
 
 private static void greet() {
